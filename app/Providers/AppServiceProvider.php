eaa8ce225b93fe976162b508b13009a5b6bec249
Your Name <you@example.com>
2024-09-26 17:29:22 -0500

    dsadiu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,12 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (config('app.env') === 'production') {
-            $scheme = request()->getScheme();
-            if ($scheme === 'http') {
-                URL::forceScheme('http');
-            } elseif ($scheme === 'https') {
+
                 URL::forceScheme('https');
-            }
+            
         }
 
         //
